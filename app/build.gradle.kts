f79e5ad9910ffef94f50164ba51318e127a26c92
Nagma Bano <nagma.bano@sap.com>
2024-02-03 15:58:21 +0530

    running app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.learning.unitconverter"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.learning.unitconverter"
