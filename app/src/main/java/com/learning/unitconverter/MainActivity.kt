2dedb0a33e39a6912f4546ca6fbeaa3b7330d871
Nagma Bano <nagma.bano@sap.com>
2024-02-04 14:06:19 +0530

    Outlined text field and anonymous function

--- app/src/main/java/com/learning/unitconverter/MainActivity.kt
+++ app/src/main/java/com/learning/unitconverter/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.learning.unitconverter
 
 import android.os.Bundle
@@ -6,7 +8,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,26 +39,19 @@ class MainActivity : ComponentActivity() {
 fun UnitConverter() {
     Column {
         // Here all the UI element will be stacked below each other
-
+        Text(text = "Unit Converter")
+        OutlinedTextField(value = "", onValueChange = {
+            // what should happen when the value of outlined text field changes
+        })
         Row {
             // Here all the UI element will be stacked next to each other
-            Greeting("Android")
-            Greeting("Android")
+
         }
     }
 }
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     UnitConverterTheme {
-        Greeting("Android")
     }
 }
