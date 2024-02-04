1f7efde2ddaf8587df0373362c57b3400addb6b6
Nagma Bano <nagma.bano@sap.com>
2024-02-04 14:23:13 +0530

    button, on click and toast

--- app/src/main/java/com/learning/unitconverter/MainActivity.kt
+++ app/src/main/java/com/learning/unitconverter/MainActivity.kt
@@ -1,13 +1,13 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.learning.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -15,6 +15,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.learning.unitconverter.ui.theme.UnitConverterTheme
 
@@ -35,6 +36,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UnitConverter() {
     Column {
@@ -43,8 +45,13 @@ fun UnitConverter() {
         OutlinedTextField(value = "", onValueChange = {
             // what should happen when the value of outlined text field changes
         })
+        Text(text = "Result:")
         Row {
+            val context = LocalContext.current
             // Here all the UI element will be stacked next to each other
+            Button(onClick = { Toast.makeText(context, "Thanks for clicking", Toast.LENGTH_LONG).show() }) {
+                Text(text = "Click Me!")
+            }
 
         }
     }
