8d2f6e2325ab33d01d631920d7653226c4f24bd4
Nagma Bano <nagma.bano@sap.com>
2024-02-04 14:44:04 +0530

    modifier, verticalArrangement and horizontalAlignment

--- app/src/main/java/com/learning/unitconverter/MainActivity.kt
+++ app/src/main/java/com/learning/unitconverter/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,6 +19,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,13 +45,14 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UnitConverter() {
-    Column {
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
         // Here all the UI element will be stacked below each other
         Text(text = "Unit Converter")
         OutlinedTextField(value = "", onValueChange = {
             // what should happen when the value of outlined text field changes
         })
-        Text(text = "Result:")
         Row {
             Box {
                 Button(onClick = { /*TODO*/ }) {
@@ -70,6 +73,7 @@ fun UnitConverter() {
 //            }
 
         }
+        Text(text = "Result:")
     }
 }
 @Preview(showBackground = true)
